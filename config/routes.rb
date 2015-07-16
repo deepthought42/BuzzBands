ba703e474142a5f9f6a6143982a0b8b1591a0052
Brandon Kindred <brandon.kindred@gmail.com>
2015-07-15 20:22:24 -0400

    added routing for venues

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :application
 
   resources :promotions
-  resources :promoters
+  resources :venues
 
   get '*path' => redirect('/')
   # The priority is based upon order of creation: first created -> highest priority.
