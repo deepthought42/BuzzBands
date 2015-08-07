0a378d5308a5e2bbee95dd538c35470dcda95a73
Brandon Kindred <brandon.kindred@gmail.com>
2015-08-06 20:05:19 -0400

    added authentication requirement to venues and promotions

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,4 +1,5 @@
 class PromotionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_promotion, only: [:show, :edit, :update, :destroy]
 
   # GET /promotions
