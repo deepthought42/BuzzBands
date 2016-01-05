d3d4a349aa4c6f500970f6cd5bb62ceffdbc5e65
Brandon Kindred <brandon.kindred@gmail.com>
2016-01-04 21:39:00 -0500

    deleted Package model file

--- app/controllers/package_controller.rb
+++ app/controllers/package_controller.rb
@@ -4,7 +4,27 @@ class PackageController < ApplicationController
   # GET /promotions
   # GET /promotions.json
   def index
-    @packages = Package.all
+    @packages = [{
+      id: 0,
+      count: 250,
+      price: '25.00'
+    },{
+      id: 1,
+      count: 500,
+      price: '50.00'
+    },{
+      id: 2,
+      count: 750,
+      price: '75.00'
+    },{
+      id: 3,
+      count: 1000,
+      price: '100.00'
+    },{
+      id: 4,
+      count: 10000,
+      price: '1000.00'
+    }]
     render json: @packages
   end
 end
