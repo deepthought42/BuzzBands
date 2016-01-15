f5ce9f6d79da2cd11bb1af7880fef416d08c608e
Brandon Kindred <brandon.kindred@gmail.com>
2016-01-14 22:39:36 -0500

    added validation to promotions and venues

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,3 +1,11 @@
 class Promotion < ActiveRecord::Base
   belongs_to :venue
+  validates_associated :venue
+
+  validates :name, presence: true, length: {minimum: 5}
+  validates :ad_location, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :venue_id, presence: true, numericality: { only_integer: true }
+  validates :description, length: {minimum: 10}, allow_blank: true
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,10 @@ class Venue < ActiveRecord::Base
   has_many :promotions
   has_many :user_venues
   has_many :users, :through => :user_venues
+
+  validates :name, presence: true, length: {minimum: 5}
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
 end
