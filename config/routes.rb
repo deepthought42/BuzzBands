d2f39e3f38906c7b1ccddbf4fdcf6d711dcf8d7b
Brandon Kindred <brandon.kindred@gmail.com>
2016-01-17 17:38:01 -0500

    fixed registration issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'api/auth', :controllers => { :registrations => "registrations" }
+  mount_devise_token_auth_for 'User', at: 'api/auth' #, :controllers => { :registrations => "registrations" }
 
   resources :application
 
