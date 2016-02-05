28e70e98194376adad1735fb6570f5624e82145d
Brandon Kindred <brandon.kindred@gmail.com>
2016-02-04 21:31:48 -0500

    actually fixed registration process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
   #protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  after_filter :set_csrf_cookie_for_ng
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  protected
+
+  # In Rails 4.1 and below
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,20 +1,18 @@
-class RegistrationsController < Devise::RegistrationsController
+class RegistrationsController < DeviseTokenAuth::RegistrationsController
+
   def create
     super
-
     if params[:venue_id]
       @venue = Venue.find(params[:venue_id])
       logger.info "VENUES FOR USER :: #{@venue.name}"
       @user.venues.push(@venue)
     end
-
-    @user.save
   end
 
   protected
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role, :venue, :uid, :provider, :venue_id)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role, :venue, :provider, :venue_id)
   end
 
   def account_update_params
