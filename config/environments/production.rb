01defc568d5c108e467e114b9e1aa3a7ac93c373
Brandon Kindred <brandon.kindred@gmail.com>
2016-08-14 17:25:59 -0400

    activated forgery protection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'hypedrive-api.heroku.com',
+    :domain         => 'hypedrive-api.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
