0db1c9797fc4a0f91b15d73ea12c157bb4fe0e7d
Brandon Kindred <brandon.kindred@gmail.com>
2016-09-03 11:43:31 -0400

    altered user scope

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,5 +1,12 @@
 class UserPolicy < ApplicationPolicy
   class Scope < Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+       @user = user
+       @scope = scope
+    end
+
     def resolve
       if user.hypedrive_employee?
         scope.all
