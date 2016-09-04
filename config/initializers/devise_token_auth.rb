65aab6e50a7891c35908f89b15a4d95fdd1d28ca
Brandon Kindred <brandon.kindred@gmail.com>
2016-09-04 14:43:45 -0400

    changed config order for devise token auth

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -5,10 +5,11 @@ DeviseTokenAuth.setup do |config|
   # each request.
   config.change_headers_on_each_request = false
   config.default_confirm_success_url = "confirmed"
+  config.token_lifespan = 24.hours
   remove_tokens_after_password_reset = true
   # By default, users will need to re-authenticate after 2 weeks. This setting
   # determines how long tokens will remain valid after they are issued.
-  config.token_lifespan = 24.hours
+
 
   # Sometimes it's necessary to make several requests to the API at the same
   # time. In this case, each request in the batch will need to share the same
