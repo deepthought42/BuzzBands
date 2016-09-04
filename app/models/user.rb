bab4d66b791d87d6ba01bf22cda95865c753336e
Brandon Kindred <brandon.kindred@gmail.com>
2016-09-04 15:30:22 -0400

    removed random uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,5 @@ class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :confirmable
-  before_save -> do
-     self.uid = SecureRandom.uuid
-#     skip_confirmation!
-   end
+
 end
